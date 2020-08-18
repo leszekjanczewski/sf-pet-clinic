337d070e4d3373e10fa7fc68bb1ad02770910153
Leszek Janczewski <leszek.janczewski@gmail.com>
2020-08-18 18:33:15 +0200

    Add speciality entity and associate to vet. Closes #26

---
+++ pet-clinic-data/src/main/java/pl/robous/sfpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package pl.robous.sfpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
