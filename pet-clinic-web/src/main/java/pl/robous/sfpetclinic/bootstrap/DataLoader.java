247b69a6199aa9aab9395fa294ee1d367f01f7bf
Leszek Janczewski <leszek.janczewski@gmail.com>
2020-08-18 10:15:17 +0200

    Refactor Map Impl to abstract ID. Closes #23

--- pet-clinic-web/src/main/java/pl/robous/sfpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pl/robous/sfpetclinic/bootstrap/DataLoader.java
@@ -22,26 +22,22 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michel");
         owner1.setLastName("Weston");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
         ownerService.save(owner2);
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("San");
         vet1.setLastName("Axe");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jim");
         vet2.setLastName("Morris");
         vetService.save(vet2);
