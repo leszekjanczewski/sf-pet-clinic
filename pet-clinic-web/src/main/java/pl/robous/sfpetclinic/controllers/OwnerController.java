404fcabd6de7d4ead86950e8f9fcf98b0cce7187
Leszek Janczewski <leszek.janczewski@gmail.com>
2020-09-28 12:25:45 +0200

    add display owner record in html. closed #53

--- pet-clinic-web/src/main/java/pl/robous/sfpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/robous/sfpetclinic/controllers/OwnerController.java
@@ -2,7 +2,10 @@ package pl.robous.sfpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import pl.robous.sfpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
@@ -26,6 +29,14 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners() {
         return "notimplemented";
+
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView view = new ModelAndView("owners/ownerDetails");
+        view.addObject(ownerService.findById(ownerId));
+        return view;
     }
 
 }
