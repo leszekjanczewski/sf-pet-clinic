46d2d388d90d21335cac03e0c66d8662d75eee23
Leszek Janczewski <leszek.janczewski@gmail.com>
2020-10-01 12:39:18 +0200

    aded json vets endpoint, closes #67

--- pet-clinic-web/src/main/java/pl/robous/sfpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/robous/sfpetclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package pl.robous.sfpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.robous.sfpetclinic.model.Vet;
 import pl.robous.sfpetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -21,5 +26,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
